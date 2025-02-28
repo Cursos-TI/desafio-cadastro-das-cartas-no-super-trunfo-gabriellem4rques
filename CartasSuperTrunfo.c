a230737f485b13aa69b0739202424011095fb640
gabriellem4rques <osdoisladosdouniverso@gmail.com>
2025-02-27 21:19:37 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,9 @@
 #include <stdio.h>
-
-#include <stdio.h>
-   //Incluindo uma biblioteca
-
+ //Ponto de partida para execução do programa.
 int main(){
-  //Ponto de partida para execução do programa.
-
-int char[50]nome;
-int codigodacidade;
-float populacao;
-double area;
-double pib;
-int pontosturisticos;
-  //Definindo variáveis separadas para cada atributo da cidade.
-
-printf("Bem Vindo ao cadastro de cartas do Jogo Super Trunfo!")
-
-
-
-
-
-
-
-}
+  //Variáveis 
+int cidade1, cidade2;
+char 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -34,3 +15,5 @@ printf("Bem Vindo ao cadastro de cartas do Jogo Super Trunfo!")
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+   }
